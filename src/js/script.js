4d342a5399a2ee0515da6cdbb1e22119d9354bf5
matiasverguet <matias.verguet--bailly@edu.univ-fcomte.fr>
2025-12-03 09:47:39 +0100

    fini

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,19 @@
-// *** LIGHTBOX ***
+document.addEventListener('DOMContentLoaded', function () {
+    const thumbnails = document.querySelectorAll('.gallery-img');
+    const lightbox = document.querySelector('#lightbox');
+
+    thumbnails.forEach(thumbnail => {
+        thumbnail.addEventListener('click', () => {
+            const fullImgPath = thumbnail.dataset.fullImg;
+            const img = document.createElement('img');
+            img.src = fullImgPath;
+            lightbox.innerHTML = '';
+            lightbox.appendChild(img);
+            lightbox.showModal();
+        });
+    });
+
+    lightbox.addEventListener('click', () => {
+        lightbox.close();
+    });
+});
